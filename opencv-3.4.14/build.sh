b9ff820b92d340c3306bb1c9f758711a10ccf155
MURAKI Taichi <muraki.taichi@gmail.com>
2021-06-23 15:35:26 +0900

    Add build script.

--- opencv-3.4.14/build.sh
+++ opencv-3.4.14/build.sh
@@ -5,4 +5,4 @@ docker run \
   -v $(pwd):/src \
   -u $(id -u):$(id -g) \
   emscripten/emsdk:2.0.10 \
-  emcmake python3 ./opencv-3.4.14/platforms/js/build_js.py build_wasm --build_wasm
+  emcmake python3 ./opencv-3.4.14/platforms/js/build_js.py build_wasm --build_wasm --build_flags="-mno-bulk-memory"
