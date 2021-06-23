dce64dc2b9cbadfecc345495b25bca3c581f7f4f
MURAKI Taichi <muraki.taichi@gmail.com>
2021-06-23 14:47:05 +0900

    Add a build script.

---
+++ opencv-3.4.14/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run \
+  --rm \
+  -v $(pwd):/src \
+  -u $(id -u):$(id -g) \
+  emscripten/emsdk:2.0.10 \
+  emcmake python3 ./opencv-3.4.14/platforms/js/build_js.py build_wasm --build_wasm
